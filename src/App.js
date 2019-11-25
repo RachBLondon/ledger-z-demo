9182cd270b56c68d3e3e2034ab8ca13461926296
Rachel Black <rachblondon@gmail.com>
2019-11-25 14:37:51 +0100

    netlify demo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
 import Box from "3box";
-import ChatBox from '3box-chatbox-react';
+import ChatBox from "3box-chatbox-react";
 
-const getThreeBox = async (address) => {
+const getThreeBox = async address => {
   const profile = await Box.getProfile(address);
   return profile;
 };
 
 export default class App extends Component {
-
   state = {
-    needToAWeb3Browser : false,
-  }
+    needToAWeb3Browser: false
+  };
   async getAddressFromMetaMask() {
     if (typeof window.ethereum == "undefined") {
       this.setState({ needToAWeb3Browser: true });
@@ -25,20 +24,31 @@ export default class App extends Component {
     await this.getAddressFromMetaMask();
     if (this.state.accounts) {
       //Demo goes here
+      const box = await Box.openBox(this.state.accounts[0], window.ethereum);
+      this.setState({ box });
+      await this.state.box.syncDone;
 
-
+      const space = await this.state.box.openSpace("ledger-z-chatbox");
+      this.setState({ space });
     }
   }
   render() {
-
-    if(this.state.needToAWeb3Browser){
-      return <h1>Please install metamask</h1>
+    if (this.state.needToAWeb3Browser) {
+      return <h1>Please install metamask</h1>;
     }
 
     return (
-        <div>
-
-        </div>
+      <div style={{width : '364px', margin : 'auto', paddingTop : '10%', textAlign : "center"}}>
+        <h1>Ledger Z ChatBox</h1>
+        {this.state.space && (
+          <ChatBox
+            spaceName="namespace-chatbox"
+            threadName="chatbox-thread"
+            box={this.state.box}
+            currentUserAddr={this.state.account}
+          />
+        )}
+      </div>
     );
   }
 }
